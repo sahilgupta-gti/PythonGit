cb210e85f455bc74e270e2f1d6693b13b0218edf
sahilgupta-gti <sahilgupta_gti@yahoo.com>
2019-03-03 18:34:22 +0530

    a

--- second.py
+++ second.py
@@ -1,4 +1,3 @@
-#! /bin/python
 print('This is second 1.2v of PythonGit')
 print('This is second 1.2v of PythonGit')
 print('Change on github')
@@ -7,5 +6,24 @@ print("change on 09-jan-2019")
 print("change on 10-jan-2019-2")
 print("change on github")
 print("change on ide")
-hi hello)
-the
+print('Python\'s')
+isHot = False
+if isHot:
+    print('hot day')
+elif isHot:
+    print('cold day')
+
+
+f = open('Myfile.txt', 'r')
+while True:
+    a = f.readline()
+    if a != '':
+        print(a, end='')
+    else:
+        print()
+        break
+# noinspection PyBroadException
+try:
+    print(4/0)
+except Exception:
+    print("you cannot divide by zero")
